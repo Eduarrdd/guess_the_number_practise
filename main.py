ac6ce8b7a633b5a9789516b5e4a150d88e023940
Eduardo <eduar2011.er36@gmail.com>
2024-04-05 12:01:05 -0600

    Defining the function

---
+++ main.py
@@ -0,0 +1,5 @@
+# First, we need to definy the function. This function will have the logic of the game. 
+
+def guess_the_number(x):
+
+# The function will have a parameter 'x' and will represent the superior limit of values 
